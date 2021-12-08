8a305c30110a0a602fc93dde06a212cb1d5125b6
aamir <aamir.1705@gmail.com>
2021-12-08 11:23:24 -0500

    push

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-<<<<<<< Updated upstream
 const add = (num1, num2) => num1 + num2
 
 add(10, 15)
-=======
-<<<<<<< HEAD
-function aamirfunction(fun) {
-    return fun
-}
-function add(no1, no2){
-    return no1 + no2
-}
-=======
-const add = (num1, num2) => num1 + num2
-
-add(10, 15)
->>>>>>> 889a85bf66e615679ed7801b383bc6289800203f
->>>>>>> Stashed changes
